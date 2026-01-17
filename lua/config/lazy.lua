61e4dafb2a72e8791f76461213777bdb97086723
Norbert Parti <norbert.parti.work@gmail.com>
2026-01-17 21:15:17 +0100

    Add terminal toggle

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,24 +25,26 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
-    -- import your plugins
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		-- import your plugins
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
+		{ import = "lazyvim.plugins.extras.lang.rust" },
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
+		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 
-    -- === DEBUGGING (DAP) EXTRAS ===
-    -- 1. Imports the core DAP functionality and setup (nvim-dap)
-    { import = "lazyvim.plugins.extras.dap.core" },
+		-- === DEBUGGING (DAP) EXTRAS ===
+		-- 1. Imports the core DAP functionality and setup (nvim-dap)
+		{ import = "lazyvim.plugins.extras.dap.core" },
 
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-  library = { "nvim-dap-ui" },
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	-- install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+	library = { "nvim-dap-ui" },
 })
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	cmd = { "ToggleTerm", "TermExec" },
+	keys = {
+		{ "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+		{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float Terminal" },
+		{ "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal Terminal" },
+		{ "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Vertical Terminal" },
+	},
+	opts = {
+		size = 20,
+		-- Using standard quotes for the mapping
+		open_mapping = "<C-\\>",
+		hide_numbers = true,
+		shade_filetypes = {},
+		shade_terminals = true,
+		start_in_insert = true,
+		insert_mappings = true,
+		persist_size = true,
+		direction = "float",
+		close_on_exit = true,
+		shell = vim.o.shell,
+		float_opts = {
+			border = "curved",
+			winblend = 0,
+		},
+	},
+}
